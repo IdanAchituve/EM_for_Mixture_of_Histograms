ddfa70444af371a72f3f02e5d1d1b723a4e58224
idan achituve <idanachi@gmail.com>
2019-01-02 19:08:35 +0200

    6th

--- data_analysis.py
+++ data_analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import utils
 
-min_prob = 0.05
+min_prob = 0.0000001
 
 def likelihood_and_perplexity(articles, log_likelihood):
 
@@ -24,7 +24,7 @@ def likelihood_and_perplexity(articles, log_likelihood):
 
     # plot the perplexity
     path = "./graphs/perplexity.png"
-    perplexity = 2**((-1/float(num_words)) * log_likelihood)
+    perplexity = np.e**((-1/float(num_words)) * log_likelihood)
     plt.plot(x_axis, perplexity)
     plt.ylabel('Perplexity')
     plt.xlabel('Iteration')
@@ -32,7 +32,7 @@ def likelihood_and_perplexity(articles, log_likelihood):
     plt.close()
 
 
-def confusion_matrix(w, articles, articles_headers):
+def confusion_matrix(path, w, articles, articles_headers):
 
     N = len(articles)  # number of documents
     clusters_per_doc = w.copy()
@@ -44,7 +44,7 @@ def confusion_matrix(w, articles, articles_headers):
 
     # count documents associate with each cluster
     num_docs_in_clusters = np.sum(clusters_per_doc, axis=0)
-    conf_matrix = np.zeros((9,10))
+    conf_matrix = np.zeros((9, 10))
 
     for doc in range(N):
 
@@ -63,5 +63,5 @@ def confusion_matrix(w, articles, articles_headers):
 
     conf_matrix[:, -1] = num_docs_in_clusters  # on the last column put the number of documents
     mat = pd.DataFrame(conf_matrix, columns=list(topics.keys()) + ["Number Of Articles"])
-    mat.sort_values("Number Of Articles", inplace=True, ascending=False)
-    ccc = 1
+    sorted_mat = mat.sort_values("Number Of Articles", ascending=False)
+    sorted_mat.to_csv(path)
